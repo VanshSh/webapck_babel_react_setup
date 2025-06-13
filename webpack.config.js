836b7d6ee77d7b07b6d4f11adb2077cc1efe9779
VanshSh <vanshsharma9354@gmail.com>
2025-06-13 08:56:51 +0530

    feat: initialize React app with Webpack and Babel setup
    - Added package.json with dependencies for React, Babel, and Webpack.
    - Created index.html for the application entry point.
    - Implemented App component with functionality for a Todo app including add, edit, delete, and toggle complete features.
    - Set up index.js to render the App component.
    - Configured Webpack with entry, output, module rules, and dev server settings.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+}
